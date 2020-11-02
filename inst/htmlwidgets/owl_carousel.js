69e7aef728a6e2abb7efc2f3efc93f446c42d9ff
Abdessabour Moutik <abds.papa@gmail.com>
2020-11-02 12:28:29 -0800

    Added Js-R interface

---
+++ inst/htmlwidgets/owl_carousel.js
@@ -0,0 +1,34 @@
+HTMLWidgets.widget({
+
+  name: 'owl_carousel',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+        // TODO: code to render the widget, e.g.
+        el.innerHTML = x.message;
+        print( el.innerHTML);
+        $(".slider").owlCarousel({
+          loop: true,
+          autoplay: true,
+          autoplayTimeout: 2000, //2000ms = 2s;
+          autoplayHoverPause: true,
+        })
+
+      },
+
+      resize: function(width, height) {
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
